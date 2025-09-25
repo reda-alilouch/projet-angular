da3dccf432c0e47cf4b1a3d6ae001c0a1b1565d2
reda alilouch <alilouchreda328black@gmail.com>
2025-09-25 16:21:28 +0100

    ajoute d' autre snap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { FaceSnap } from './face-snap/face-snap';
-
+import { Facesnap } from './models/face-snap';
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
@@ -8,5 +8,31 @@ import { FaceSnap } from './face-snap/face-snap';
   standalone: true,
   imports: [FaceSnap],
 })
-export class App {
+export class App implements OnInit{
+  mySnap! : Facesnap;
+  myOtherSnap! : Facesnap;
+  myLastSnap!: Facesnap;
+  ngOnInit(): void {
+      this.mySnap = new Facesnap(
+        "Mon premier FaceSnap",
+        "Mon premier FaceSnap description",
+        new Date(),
+        0,
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+      );
+      this.myOtherSnap = new Facesnap(
+        "Mon deuxième FaceSnap",
+        "Mon deuxième FaceSnap description",
+        new Date(),
+        5,
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+      );
+      this.myLastSnap = new Facesnap(
+        "Mon troisième FaceSnap",
+        "Mon troisième FaceSnap description",
+        new Date(),
+        10,
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+      );
+  }
 }
--- src/app/face-snap/face-snap.ts
+++ src/app/face-snap/face-snap.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Facesnap } from '../models/face-snap';
 
 @Component({
   selector: 'app-face-snap',
@@ -6,32 +7,28 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './face-snap.html',
   styleUrls: ['./face-snap.scss']
 })
-export class FaceSnap implements OnInit{
-  title!: string;
-  description!: string;
-  createdAt!: Date;
-  snaps!: number;
-  imageUrl!: string;
-  snapButtonText!: string;
-  userHasSnaped!: boolean;
-  ngOnInit(): void {
-      this.title = "Mon premier FaceSnap";
-      this.description = "Mon premier FaceSnap description";
-      this.createdAt = new Date();
-      this.snaps = 0;
-      this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
-      this.snapButtonText = 'Oh Snap!';
-      this.userHasSnaped = false;
-    }
-  onSnap(): void {  
+export class FaceSnap {
+  @Input() faceSnap!: Facesnap;
+  snapButtonText: string = 'Oh Snap!';
+  userHasSnaped: boolean = false;
+
+  onSnap(): void {
     if (this.userHasSnaped) {
-      this.snaps--;
-      this.snapButtonText = 'Oh Snap!';
-      this.userHasSnaped = false;
+      this.snap();
     } else {
-      this.snaps++;
-      this.snapButtonText = 'Oops, unSnap!';
-      this.userHasSnaped = true;
+      this.unSnap();
     }
+  }
+
+  snap(): void {
+    this.faceSnap.snaps--;
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnaped = false;
+  }
+
+  unSnap(): void {
+    this.faceSnap.snaps++;
+    this.snapButtonText = 'Oops, unSnap!';
+    this.userHasSnaped = true;
   }
 }
